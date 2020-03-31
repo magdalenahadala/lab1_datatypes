9d581d09197befe9a132fdf33b187ea16e5f4e2f
Magdalena Hadała <magdalenahadala98@gmail.com>
2020-03-31 19:45:44 +0200

    push app

---
+++ Sequence.java
@@ -0,0 +1,29 @@
+package helloWorld;
+
+//abstract class Sequence,which represents a data string of any type specified by a parameter template.
+public abstract class Sequence <T> extends Packet{
+	protected int channelNr; //channel number
+	protected String unit; //unit of measured value
+	protected double resolution; //resolution in time domain or other
+	protected T[] buffer; //T-type data table
+	
+	//constructor
+	public Sequence(String _device, String _description, long _date, int _channelNr, String _unit, double _resolution, T[] _table){
+		super(_device, _description, _date);
+		this.channelNr = _channelNr;
+		this.unit = _unit;
+		this.resolution = _resolution;
+		this.buffer = _table;
+		}
+	// method returning a string containing the object's attribute values
+	public String toString(){
+		String result_pack;
+		String result_seq; result_pack = super.toString();
+		String temp = "";
+		for (int i = 0; i < buffer.length; i++){
+			temp = temp + buffer[i] + ", ";
+			}
+		result_seq = result_pack + ", \nChannel Number = " + channelNr + ", \nUnit = " + unit + ", \nResolultion = " + resolution + ", \nBuffer = " + temp;
+		return result_seq;
+		}
+}
